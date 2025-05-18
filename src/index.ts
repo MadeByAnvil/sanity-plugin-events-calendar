ba8c2f424800d9b49cfe9bfbe75932bd7ca40a9d
Casey Zumwalt <casey@zumwa.lt>
2025-05-17 21:48:22 -0500

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import {definePlugin} from 'sanity'
+
+interface MyPluginConfig {
+  /* nothing here yet */
+}
+
+/**
+ * Usage in `sanity.config.ts` (or .js)
+ *
+ * ```ts
+ * import {defineConfig} from 'sanity'
+ * import {myPlugin} from 'sanity-plugin-events-calendar'
+ *
+ * export default defineConfig({
+ *   // ...
+ *   plugins: [myPlugin()],
+ * })
+ * ```
+ */
+export const myPlugin = definePlugin<MyPluginConfig | void>((config = {}) => {
+  // eslint-disable-next-line no-console
+  console.log('hello from sanity-plugin-events-calendar')
+  return {
+    name: 'sanity-plugin-events-calendar',
+  }
+})
